7edadbad6ccf07b5bead68afd9bca52bbd40146c
gerssivaldosantos <canalandows@gmail.com>
2021-09-16 20:20:35 -0300

    funçao setup browser criada

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from selenium.webdriver import Chrome 
-
+import platform
 
 def setup_browser():
-    browser = Chrome()
+    """ Esta função organiza todas as chamadas
+    relacionadas à instancia do chromedriver """
+    if platform.system() == "Linux":
+        print("Rodando no Linux")
+        browser = Chrome(executable_path="./chromedriver")
+    elif platform.system() == "Windows":
+        browser = Chrome(executable_path=r".\chromedriver.exe")
 
-print()
+setup_browser()
